866d632228c3f354aa78c08d5b85f3b4197911a2
thuvasiva <77840401+thuvasiva@users.noreply.github.com>
2025-01-03 12:47:49 +0000

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,9 @@
+from src.deck import Deck
+
+
+def play():
+    print('Hello, potential future BBC developer!')  # execution starts here! remove this print() and add your own code.
+
+
+if __name__ == '__main__':
+    play()
---
+++ test/test_deck.py
@@ -0,0 +1,19 @@
+import unittest
+from src.deck import Deck
+
+
+class DeckTestCase(unittest.TestCase):
+
+    def setUp(self):  # this method will be run before each test
+        self.deck = Deck()
+
+    def tearDown(self):  # this method will be run after each test
+        pass
+
+    def test_number_of_cards(self):  # any method beginning with 'test_' will be run by unittest
+        number_of_cards = len(self.deck.cards)
+        self.assertEqual(number_of_cards, 52)
+
+
+if __name__ == '__main__':
+    unittest.main()
